dafedd4cb97b154b7bdc64a627892606408b9468
951595 <107821916+951595@users.noreply.github.com>
2024-03-14 16:21:48 +0530

    Update LtimgenieGithubApplication.java

--- src/main/java/com/example/demo/LtimgenieGithubApplication.java
+++ src/main/java/com/example/demo/LtimgenieGithubApplication.java
@@ -1,8 +1,7 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,9 +11,9 @@ public class LtimgenieGithubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LtimgenieGithubApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String Hello() {
-		return "welcome to LTIMindtree";
+	@RequestMapping("/")
+        public String home() {
+                return "Hello World!";
 	}
 
 }
